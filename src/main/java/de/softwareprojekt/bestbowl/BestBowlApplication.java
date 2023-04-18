2fa511634a24ecaf9dd64c6e2f6dcc9f10e0e479
martenvoss <mvoss1999@googlemail.com>
2023-04-18 18:07:15 +0200

    initial commit

---
+++ src/main/java/de/softwareprojekt/bestbowl/BestBowlApplication.java
@@ -0,0 +1,13 @@
+package de.softwareprojekt.bestbowl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestBowlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestBowlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/softwareprojekt/bestbowl/BestBowlApplicationTests.java
@@ -0,0 +1,13 @@
+package de.softwareprojekt.bestbowl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestBowlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
