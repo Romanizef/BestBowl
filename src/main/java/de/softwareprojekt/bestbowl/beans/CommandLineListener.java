08307c139fa0cd0ef3d300368ab9bf444d6dc0d6
martenvoss <49848311+martenvoss@users.noreply.github.com>
2023-04-26 17:40:03 +0200

    generierte getränkenamen fix

--- src/main/java/de/softwareprojekt/bestbowl/beans/CommandLineListener.java
+++ src/main/java/de/softwareprojekt/bestbowl/beans/CommandLineListener.java
@@ -170,7 +170,7 @@ public class CommandLineListener {
         Faker faker = new Faker();
         for (int i = 0; i < count; i++) {
             Drink drink = new Drink();
-            drink.setName(faker.food().dish());
+            drink.setName(faker.beer().yeast());
             drink.setStockInMilliliters(faker.random().nextInt(15, 45) * 1000);
             drink.setReorderPoint(5000);
             double price = faker.random().nextInt(1, 3);
