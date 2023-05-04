34acaff3f201c05203b8168d8110daca240f3abb
Matija Kopschek <129887675+Romanizef@users.noreply.github.com>
2023-05-05 01:43:12 +0200

    RechnungsPDF

--- src/main/java/de/softwareprojekt/bestbowl/utils/PDFUtils.java
+++ src/main/java/de/softwareprojekt/bestbowl/utils/PDFUtils.java
@@ -6,6 +6,9 @@ import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.awt.Color;
+
+import de.softwareprojekt.bestbowl.jpa.entities.Client;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,12 +19,20 @@ import java.io.IOException;
  */
 public class PDFUtils {
     private static final Logger LOGGER = LoggerFactory.getLogger(PDFUtils.class);
+    private Client client;
 
-    private PDFUtils() {
+    /**
+     * 
+     */
+    public PDFUtils() {
     }
 
-    public static void createDemoPdf() {
-        //create "test" folder in working dir
+
+    /**
+     * @throws IOException
+     */
+    public void createInvoicePdf() throws IOException {
+        // create "test" folder in working dir
         File testFolder = new File(Utils.getWorkingDirPath() + File.separator + "rechnung");
         if (!Utils.createDirectoryIfMissing(testFolder)) {
             return;
@@ -35,15 +46,104 @@ public class PDFUtils {
             document.addPage(page);
 
             PDPageContentStream contentStream = new PDPageContentStream(document, page);
+
+            // int pageWidth = (int) page.getTrimBox().getWidth(); // get width of the page
+            int pageHeight = (int) page.getTrimBox().getHeight(); // get height of the page
+
+            contentStream.setStrokingColor(Color.BLUE);
+            contentStream.setLineWidth(1);
+
+            int initRectCoordinateX = 50;
+            int initRectCoordinateY = pageHeight - 70;
+
+            int cellHeight = 20;
+            int cellWidth = 150;
+
+            int colCount = 2;
+            int rowCount = 5;
+
+            for (int i = 1; i <= rowCount; i++) {
+                for (int j = 1; j <= colCount; j++) {
+                    if (j == 2) {
+                        contentStream.addRect(initRectCoordinateX, initRectCoordinateY, cellWidth, cellHeight);
+                        initRectCoordinateX += cellWidth;
+                    } else {
+                        contentStream.addRect(initRectCoordinateX, initRectCoordinateY, cellWidth, cellHeight);
+                        initRectCoordinateX += cellWidth;
+                    }
+                }
+                initRectCoordinateX = 50;
+                initRectCoordinateY -= cellHeight;
+            }
+
+            int initTextCoordinateX = 50;
+
+            // Überschrift
             contentStream.beginText();
-            contentStream.setLeading(14.5f);
+            contentStream.newLineAtOffset(initTextCoordinateX - 20, pageHeight - 30);
             contentStream.setFont(PDType1Font.HELVETICA_BOLD, 20);
-            contentStream.newLineAtOffset(25, 700);
-            //contentStream.showText("Rechnungsnr" + );
-            contentStream.newLine();
-            contentStream.setFont(PDType1Font.HELVETICA, 15);
-            contentStream.showText("test");
+            contentStream.showText("BestBowl Rechnung");
             contentStream.endText();
+
+            // Tabelleninhalt
+
+            // Linke Spalte
+            contentStream.beginText();
+            contentStream.setLeading(20f);
+            contentStream.setFont(PDType1Font.TIMES_BOLD, 10);
+            contentStream.newLineAtOffset(initTextCoordinateX + 5, pageHeight - 64);
+
+            contentStream.showText("Rechnungsnummer:");
+
+            contentStream.newLine();
+            contentStream.showText("Kundenid:");
+
+            contentStream.newLine();
+            contentStream.showText("Kunden Nachname:");
+
+            contentStream.newLine();
+            contentStream.showText("Datum:");
+
+            contentStream.newLine();
+            contentStream.showText("Summe:");
+            contentStream.endText();
+
+            // Rechte Spalte
+            contentStream.beginText();
+            contentStream.newLineAtOffset(initTextCoordinateX + 160, pageHeight - 65);
+            contentStream.setFont(PDType1Font.TIMES_ROMAN, 10);
+            contentStream.showText("!");
+
+            contentStream.newLine();
+            contentStream.showText("12345");
+
+            contentStream.newLine();
+            contentStream.showText("Max Mustermann");
+
+            contentStream.newLine();
+            contentStream.showText("01.01.2023");
+
+            contentStream.newLine();
+            contentStream.showText("25,50");
+            contentStream.endText();
+
+            // Impressum
+            contentStream.beginText();
+            contentStream.setFont(PDType1Font.HELVETICA, 7);
+            contentStream.setLeading(14.5f);
+            contentStream.newLineAtOffset(initTextCoordinateX - 30, pageHeight - 700);
+            contentStream.showText("BestBowl GmbH");
+            contentStream.newLine();
+            contentStream.showText("Interaktion 1");
+            contentStream.newLine();
+            contentStream.showText("33619 Bielefeld");
+            contentStream.newLine();
+            contentStream.showText("Deutschland");
+            contentStream.newLine();
+            contentStream.showText("E-Mail: bestbowl11@gmail.com");
+            contentStream.endText();
+
+            contentStream.stroke();
             contentStream.close();
 
             document.save(pdfFile);
--- src/main/java/de/softwareprojekt/bestbowl/views/StatisticsView.java
+++ src/main/java/de/softwareprojekt/bestbowl/views/StatisticsView.java
@@ -5,33 +5,47 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.grid.dataview.GridListDataView;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.provider.SortDirection;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+
+import de.softwareprojekt.bestbowl.utils.PDFUtils;
 import de.softwareprojekt.bestbowl.utils.enums.UserRole;
 import jakarta.annotation.security.RolesAllowed;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
+import static de.softwareprojekt.bestbowl.utils.Utils.matchAndRemoveIfContains;
 
 /**
+ * Creates a view for all bookings to be displayed and downloaded
+ * 
  * @author Matija Kopschek
  */
 @Route(value = "statistics", layout = MainView.class)
 @PageTitle("Statistiken")
-@RolesAllowed({UserRole.OWNER, UserRole.ADMIN})
+@RolesAllowed({ UserRole.OWNER, UserRole.ADMIN })
 public class StatisticsView extends VerticalLayout {
     private Grid<Statistic> statisticGrid;
     private Statistic selectedStatistic = null;
     private TextField searchField;
+    private PDFUtils pdfUtils;
 
     public StatisticsView() {
         setSizeFull();
+        pdfUtils = new PDFUtils();
         Component searchComponent = createSearchComponent();
         HorizontalLayout gridLayout = createGridLayout();
         add(searchComponent, gridLayout, createDownloadAsPDFButton());
@@ -60,8 +74,28 @@ public class StatisticsView extends VerticalLayout {
         searchLayout.add(searchField, searchButton);
     }
 
-    //TODO Tabelle aktualisieren je nach Suchangabe
     private void updateGridItems() {
+        List<Statistic> statisticList = new ArrayList<>();
+        String searchFieldValue = searchField.getValue();
+        String[] searchTerms;
+        if (searchFieldValue != null) {
+            searchTerms = searchFieldValue.trim().split(" ");
+            Iterator<Statistic> statisticIterator = statisticList.iterator();
+            while (statisticIterator.hasNext()) {
+                Statistic statistic = statisticIterator.next();
+                List<String> searchTermsCopy = new ArrayList<>(Arrays.stream(searchTerms).toList());
+                matchAndRemoveIfContains(String.valueOf(statistic.id()), searchTermsCopy);
+                matchAndRemoveIfContains(String.valueOf(statistic.clientID()), searchTermsCopy);
+                matchAndRemoveIfContains(statistic.clientLastName(), searchTermsCopy);
+                matchAndRemoveIfContains(String.valueOf(statistic.date()), searchTermsCopy);
+                matchAndRemoveIfContains(String.valueOf(statistic.total()), searchTermsCopy);
+                if (!searchTermsCopy.isEmpty()) {
+                    statisticIterator.remove();
+                }
+            }
+        }
+        GridListDataView<Statistic> statisticGridListDataView = statisticGrid.setItems(statisticList);
+        statisticGridListDataView.setSortOrder(Statistic::clientLastName, SortDirection.ASCENDING);
     }
 
     private HorizontalLayout createGridLayout() {
@@ -72,7 +106,9 @@ public class StatisticsView extends VerticalLayout {
         return layout;
     }
 
-    //TODO Daten aus record Statistike Klasse
+    /**
+     * @return
+     */
     private Grid<Statistic> createGrid() {
         Grid<Statistic> grid = new Grid<>(Statistic.class);
         grid.setSelectionMode(Grid.SelectionMode.SINGLE);
@@ -93,12 +129,20 @@ public class StatisticsView extends VerticalLayout {
         return grid;
     }
 
+    /**
+     * @return
+     */
     private Button createDownloadAsPDFButton() {
         Button pdfButton = new Button("PDF");
         pdfButton.setIcon(new Icon(VaadinIcon.DOWNLOAD));
         pdfButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY, ButtonVariant.LUMO_LARGE);
         pdfButton.addClickListener(e -> {
-            //TODO pdf erstellen und downloaden
+            try {
+                pdfUtils.createInvoicePdf();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            // TODO pdf erstellen und downloaden
         });
         return pdfButton;
     }
