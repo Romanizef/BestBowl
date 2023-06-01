122f0e90d429a57c777687c6ae2587272b8c2b3f
Matija Kopschek <129887675+Romanizef@users.noreply.github.com>
2023-06-02 01:26:29 +0200

    PDFErstellung Zwischenstand

--- src/main/java/de/softwareprojekt/bestbowl/utils/pdf/PDFUtils.java
+++ src/main/java/de/softwareprojekt/bestbowl/utils/pdf/PDFUtils.java
@@ -1,11 +1,21 @@
 package de.softwareprojekt.bestbowl.utils.pdf;
 
+import static java.awt.Color.LIGHT_GRAY;
+import static java.awt.Color.WHITE;
+import static org.apache.pdfbox.pdmodel.font.PDType1Font.HELVETICA;
+import static org.apache.pdfbox.pdmodel.font.PDType1Font.HELVETICA_BOLD;
+import static org.apache.pdfbox.pdmodel.font.PDType1Font.HELVETICA_BOLD_OBLIQUE;
+import static org.apache.pdfbox.pdmodel.font.PDType1Font.HELVETICA_OBLIQUE;
+import static org.vandeseer.easytable.settings.HorizontalAlignment.CENTER;
+import static org.vandeseer.easytable.settings.HorizontalAlignment.LEFT;
+import static org.vandeseer.easytable.settings.HorizontalAlignment.RIGHT;
+import static org.vandeseer.easytable.settings.VerticalAlignment.TOP;
+
 import java.awt.Color;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
@@ -13,12 +23,16 @@ import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.vandeseer.easytable.TableDrawer;
+import org.vandeseer.easytable.structure.Row;
+import org.vandeseer.easytable.structure.Table;
+import org.vandeseer.easytable.structure.Table.TableBuilder;
+import org.vandeseer.easytable.structure.cell.TextCell;
 
 import de.softwareprojekt.bestbowl.beans.Repos;
 import de.softwareprojekt.bestbowl.jpa.entities.BowlingAlleyBooking;
 import de.softwareprojekt.bestbowl.jpa.entities.BowlingCenter;
 import de.softwareprojekt.bestbowl.jpa.entities.BowlingShoeBooking;
-import de.softwareprojekt.bestbowl.jpa.entities.Client;
 import de.softwareprojekt.bestbowl.jpa.entities.DrinkBooking;
 import de.softwareprojekt.bestbowl.jpa.entities.FoodBooking;
 import de.softwareprojekt.bestbowl.utils.Utils;
@@ -30,14 +44,12 @@ import de.softwareprojekt.bestbowl.utils.Utils;
 public class PDFUtils {
     private static final Logger LOGGER = LoggerFactory.getLogger(PDFUtils.class);
     private static final int CELL_HEIGT = 20;
-    private static final int CELL_WIDTH_POS = 10;
-    private static final int CELL_WIDTH_DISCRIPTION = 150;
-    private static final int CELL_WIDTH_AMOUNT = 10;
-    private static final int CELL_WIDTH_PRICE = 30;
-    private static final int CELL_WIDTH_DISCOUNT = 30;
-    private static final int CELL_WIDTH_TOTAL = 30;
     private static final int TEXT_OFFSET = 50;
     private static final int POSITIONS_PER_PAGE = 10;
+
+    private final static Color BLUE_DARK = new Color(76, 129, 190);
+    private final static Color BLUE_LIGHT_1 = new Color(186, 206, 230);
+    private final static Color BLUE_LIGHT_2 = new Color(218, 230, 242);
 
     private PDFUtils() {
     }
@@ -62,18 +74,15 @@ public class PDFUtils {
             int positionIndex = 0;
             int tableFooterOffset = (totalPositions % POSITIONS_PER_PAGE) * CELL_HEIGT;
             for (int i = 1; i <= pages; i++) {
-                PDPage pdPage = createPage(booking, bowlingCenter, document, i);
-
+                // PDPage pdPage = createPage(booking, bowlingCenter, document, i);
                 if (positionIndex < totalPositions) {
-                    List<InvoiceLine> invoiceLineList = createInvoiceLineList(drinkBookingList, foodBookingList,
+                    List<InvoiceLine> invoiceLineList = createInvoiceLineList(drinkBookingList,
+                            foodBookingList,
                             shoeBookingList, positionIndex);
-                    //createTableText(document, pdPage, invoiceLineList);
+                    PDPage pdPage = createPage(booking, bowlingCenter, document, i, tableFooterOffset, invoiceLineList);
                     positionIndex += POSITIONS_PER_PAGE;
+                    document.addPage(pdPage);
                 }
-                if (positionIndex < totalPositions) {
-                    createTableFooter(document, pdPage, tableFooterOffset);
-                }
-                document.addPage(pdPage);
             }
             document.save(out);
         } catch (IOException e) {
@@ -86,142 +95,38 @@ public class PDFUtils {
     private static List<InvoiceLine> createInvoiceLineList(List<DrinkBooking> drinkBookingList,
             List<FoodBooking> foodBookingList, List<BowlingShoeBooking> shoeBookingList, int positionIndex) {
         List<InvoiceLine> invoiceLineList = new ArrayList<>(POSITIONS_PER_PAGE);
-        for (int i = 0; i < POSITIONS_PER_PAGE; i++) {
-            int offset = 0;
-            if (positionIndex + i + offset < drinkBookingList.size()) {
+        int i = 0;
+        do {
+            if (positionIndex + i < drinkBookingList.size()) {
                 DrinkBooking drinkBooking = drinkBookingList.get(positionIndex + i);
                 InvoiceLine invoiceLine = new InvoiceLine("" + positionIndex + 1, drinkBooking.getName(),
                         "" + drinkBooking.getAmount(), "" + drinkBooking.getPrice());
                 invoiceLineList.add(invoiceLine);
             }
-            offset += drinkBookingList.size();
-            if (positionIndex + i + offset < foodBookingList.size() + offset) {
-                FoodBooking foodBooking = foodBookingList.get(positionIndex + i + offset);
+            if (positionIndex + i < foodBookingList.size()) {
+                FoodBooking foodBooking = foodBookingList.get(positionIndex + i);
                 InvoiceLine invoiceLine = new InvoiceLine("" + positionIndex + 1, foodBooking.getName(),
                         "" + foodBooking.getAmount(), "" + foodBooking.getPrice());
                 invoiceLineList.add(invoiceLine);
             }
-            offset += foodBookingList.size();
-            if (positionIndex + i + offset < shoeBookingList.size() + offset) {
-                BowlingShoeBooking shoeBooking = shoeBookingList.get(positionIndex + i + offset);
+            if (positionIndex + i < shoeBookingList.size()) {
+                BowlingShoeBooking shoeBooking = shoeBookingList.get(positionIndex + i);
                 InvoiceLine invoiceLine = new InvoiceLine("" + positionIndex + 1,
                         "Schuhgröße: " + shoeBooking.getBowlingShoe().getSize(),
-                        "1", "bla"); // TODO Schupreis noch ergänzen
+                        "" + shoeBooking.getBowlingShoe().getId(), "" + shoeBooking.getPrice());
                 invoiceLineList.add(invoiceLine);
             }
-        }
+            i++;
+        } while (i < POSITIONS_PER_PAGE);
         return invoiceLineList;
     }
 
-    private static void createTableText(PDDocument document, PDPage pdPage, List<InvoiceLine> invoiceLineList) {
-        try (PDPageContentStream contentStream = new PDPageContentStream(document, pdPage)) {
-            // int pageWidth = (int) page.getTrimBox().getWidth(); // get width of the page
-            int pageHeight = (int) pdPage.getTrimBox().getHeight(); // get height of the page
-
-            createTable(contentStream, pageHeight, invoiceLineList.size());
-
-            // Tabelleninhalt
-            // Positionsnummer
-            contentStream.beginText();
-            contentStream.setLeading(20f);
-            contentStream.setFont(PDType1Font.TIMES_BOLD, 10);
-            // contentStream.newLineAtOffset(initTextCoordinateX + 5, pageHeight - 64);
-            contentStream.endText();
-
-            // Rechte Spalte
-            contentStream.beginText();
-            contentStream.newLineAtOffset(TEXT_OFFSET + 160, pageHeight - 65);
-            contentStream.setFont(PDType1Font.TIMES_ROMAN, 10);
-            contentStream.showText("!");
-
-            contentStream.newLine();
-            contentStream.showText("12345");
-
-            contentStream.newLine();
-            contentStream.showText("Max Mustermann");
-
-            contentStream.newLine();
-            contentStream.showText("01.01.2023");
-
-            contentStream.newLine();
-            contentStream.showText("25,50");
-            contentStream.endText();
-
-            contentStream.stroke();
-        } catch (IOException e) {
-            LOGGER.error(e.getMessage());
-        }
-    }
-
-    private static void createTable(PDPageContentStream contentStream, int pageHeight, int rowCount)
-            throws IOException {
-        contentStream.setStrokingColor(Color.BLACK);
-        contentStream.setLineWidth(1);
-
-        // TabellenHeader
-        int offset = TEXT_OFFSET + 5;
-        contentStream.beginText();
-        contentStream.setLeading(20f);
-        contentStream.setFont(PDType1Font.TIMES_BOLD, 10);
-        contentStream.newLineAtOffset(offset, pageHeight - 100);
-        contentStream.showText("PosNr");
-
-        offset += CELL_WIDTH_POS;
-        contentStream.newLineAtOffset(offset, pageHeight - 100);
-        contentStream.showText("Beschreibung");
-
-        offset += CELL_WIDTH_DISCRIPTION;
-        contentStream.newLineAtOffset(offset, pageHeight - 100);
-        contentStream.showText("Anzahl");
-
-        offset += CELL_WIDTH_AMOUNT;
-        contentStream.newLineAtOffset(offset, pageHeight - 100);
-        contentStream.showText("Preis");
-        contentStream.endText();
-
-        int initRectCoordinateX = 50;
-        int initRectCoordinateY = pageHeight - 70;
-
-        for (int i = 1; i <= rowCount; i++) {
-            contentStream.addRect(initRectCoordinateX, initRectCoordinateY, CELL_WIDTH_POS, CELL_HEIGT);
-            initRectCoordinateX += CELL_WIDTH_POS;
-            contentStream.addRect(initRectCoordinateX, initRectCoordinateY, CELL_WIDTH_DISCRIPTION, CELL_HEIGT);
-            initRectCoordinateX += CELL_WIDTH_DISCRIPTION;
-            contentStream.addRect(initRectCoordinateX, initRectCoordinateY, CELL_WIDTH_AMOUNT, CELL_HEIGT);
-            initRectCoordinateX += CELL_WIDTH_AMOUNT;
-            contentStream.addRect(initRectCoordinateX, initRectCoordinateY, CELL_WIDTH_PRICE, CELL_HEIGT);
-            initRectCoordinateX += CELL_WIDTH_PRICE;
-
-            initRectCoordinateX = 50;
-            initRectCoordinateY -= CELL_HEIGT;
-        }
-    }
-
-    private static void createTableFooter(PDDocument document, PDPage pdPage, int tableFooterOffset) {
-        try (PDPageContentStream contentStream = new PDPageContentStream(document, pdPage)) {
-            // int pageWidth = (int) page.getTrimBox().getWidth(); // get width of the page
-            int pageHeight = (int) pdPage.getTrimBox().getHeight(); // get height of the page
-
-            // TextFooter
-            // TODO Summe und Rabatt Textfelder und Tabelle
-            contentStream.beginText();
-            contentStream.setLeading(20f);
-            contentStream.setFont(PDType1Font.TIMES_BOLD, 10);
-            // contentStream.newLineAtOffset(initTextCoordinateX + 5, pageHeight - 64);
-            contentStream.endText();
-            contentStream.stroke();
-        } catch (IOException e) {
-            LOGGER.error(e.getMessage());
-        }
-    }
-
     private static PDPage createPage(BowlingAlleyBooking booking, BowlingCenter bowlingCenter, PDDocument document,
-            int pageNumber) {
+            int pageNumber, int tableFooterOffset, List<InvoiceLine> invoiceLineList) {
         PDPage page = new PDPage();
         try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
             // int pageWidth = (int) page.getTrimBox().getWidth(); // get width of the page
             int pageHeight = (int) page.getTrimBox().getHeight(); // get height of the page
-
             contentStream.setStrokingColor(Color.BLUE);
             contentStream.setLineWidth(1);
 
@@ -237,7 +142,7 @@ public class PDFUtils {
             contentStream.beginText();
             contentStream.setLeading(14.5f);
             contentStream.setFont(PDType1Font.HELVETICA, 10);
-            contentStream.newLineAtOffset(TEXT_OFFSET, pageHeight - 64);
+            contentStream.newLineAtOffset(TEXT_OFFSET, pageHeight - 65);
             contentStream.showText("Rechnungsempfänger");
             contentStream.newLine();
             contentStream.showText(booking.getClient().getFullName());
@@ -253,13 +158,25 @@ public class PDFUtils {
             contentStream.beginText();
             contentStream.setLeading(14.5f);
             contentStream.setFont(PDType1Font.HELVETICA, 10);
-            contentStream.newLineAtOffset(TEXT_OFFSET + 400, pageHeight - 64);
-            contentStream.showText("" + booking.getId());
+            contentStream.newLineAtOffset(TEXT_OFFSET + 350, pageHeight - 65);
+            contentStream.showText("Rechnungsnummer: " + booking.getId());
             contentStream.newLine();
-            contentStream.showText("" + booking.getClient().getId());
+            contentStream.showText("Kundennummer: " + booking.getClient().getId());
             contentStream.newLine();
-            contentStream.showText("" + Utils.toDateOnlyString(booking.getStartTime()));
+            contentStream.showText("Datum: " + Utils.toDateOnlyString(booking.getStartTime()));
             contentStream.endText();
+
+            // Tabelle
+
+            // Set up the drawer
+            TableDrawer tableDrawer = TableDrawer.builder()
+                    .contentStream(contentStream)
+                    .startX(50f)
+                    .startY(page.getMediaBox().getUpperRightY() - 140f)
+                    .table(createSimpleExampleTable())
+                    .build();
+            tableDrawer.draw();
+            // Ende Tabelle
 
             // Footer
             contentStream.beginText();
@@ -277,15 +194,111 @@ public class PDFUtils {
             contentStream.showText(bowlingCenter.getEmail());
             contentStream.endText();
 
+            // Seitennummer
+            contentStream.beginText();
+            contentStream.setLeading(14.5f);
+            contentStream.setFont(PDType1Font.HELVETICA_BOLD, 7);
+            contentStream.newLineAtOffset(TEXT_OFFSET + 500, pageHeight - 750);
+            contentStream.showText("Seite: " + pageNumber);
+            contentStream.endText();
+
             contentStream.stroke();
         } catch (IOException e) {
             LOGGER.error(e.getMessage());
         }
-
         return page;
     }
 
     private record InvoiceLine(String posnr, String description, String amount, String price) {
     };
+
+    private static Table createSimpleExampleTable() {
+        final TableBuilder tableBuilder = Table.builder()
+                .addColumnsOfWidth(100, 200, 100, 100)
+                .fontSize(10)
+                .font(HELVETICA)
+                .borderColor(Color.WHITE);
+
+        // header row
+        tableBuilder.addRow(Row.builder()
+                .add(TextCell.builder().text("Posnr").borderWidth(1).build())
+                .add(TextCell.builder().text("Beschreibung").borderWidth(1).build())
+                .add(TextCell.builder().text("Menge").borderWidth(1).build())
+                .add(TextCell.builder().text("Preis").borderWidth(1).build())
+                .backgroundColor(BLUE_DARK)
+                .textColor(Color.WHITE)
+                .font(HELVETICA_BOLD)
+                .fontSize(12)
+                .horizontalAlignment(CENTER)
+                .build());
+
+        // data rows
+        Object[][] DATA = new Object[][] {
+                { "1", 134.0, 145.0 },
+                { "2", 768.0, 677.0 },
+                { "3", 456.2, 612.0 },
+                { "4", 302.3, 467.0 }
+        };
+        double grandTotal = 0;
+        for (int i = 0; i < DATA.length; i++) {
+            final Object[] dataRow = DATA[i];
+            final double total = (double) dataRow[1] + (double) dataRow[2];
+            grandTotal += total;
+
+            tableBuilder.addRow(Row.builder()
+                    .add(TextCell.builder().text(String.valueOf(dataRow[0])).borderWidth(1)
+                            .build())
+                    .add(TextCell.builder().text(dataRow[1] + " €").borderWidth(1).build())
+                    .add(TextCell.builder().text(dataRow[2] + " €").borderWidth(1).build())
+                    .add(TextCell.builder().text(total + " €").borderWidth(1).build())
+                    .backgroundColor(i % 2 == 0 ? BLUE_LIGHT_1 : BLUE_LIGHT_2)
+                    .horizontalAlignment(CENTER)
+                    .build());
+        }
+
+        // Rabattzeile
+        tableBuilder.addRow(Row.builder()
+                .add(TextCell.builder()
+                        .text("Summe").horizontalAlignment(RIGHT)
+                        .colSpan(3)
+                        .lineSpacing(1f)
+                        .borderWidthTop(1)
+                        .textColor(WHITE)
+                        .backgroundColor(BLUE_DARK)
+                        .fontSize(7)
+                        .font(HELVETICA_OBLIQUE)
+                        .borderWidth(1)
+                        .build())
+                .add(TextCell.builder().text(grandTotal + " €").backgroundColor(LIGHT_GRAY)
+                        .font(HELVETICA_BOLD_OBLIQUE)
+                        .verticalAlignment(TOP)
+                        .borderWidth(1)
+                        .build())
+                .horizontalAlignment(CENTER)
+                .build());
+
+        // Summenzeile
+        tableBuilder.addRow(Row.builder()
+                .add(TextCell.builder()
+                        .text("Rabatt").horizontalAlignment(RIGHT)
+                        .colSpan(3)
+                        .lineSpacing(1f)
+                        .borderWidthTop(1)
+                        .textColor(WHITE)
+                        .backgroundColor(BLUE_DARK)
+                        .fontSize(7)
+                        .font(HELVETICA_OBLIQUE)
+                        .borderWidth(1)
+                        .build())
+                .add(TextCell.builder().text("10%").backgroundColor(LIGHT_GRAY)
+                        .font(HELVETICA_BOLD_OBLIQUE)
+                        .verticalAlignment(TOP)
+                        .borderWidth(1)
+                        .build())
+                .horizontalAlignment(CENTER)
+                .build());
+
+        return tableBuilder.build();
+    }
 
 }
